aa42e6d7da4e69d777f2eb0f92bf766c41c837ae
sl-matrix <sl-matrix@hotmail.com>
2017-10-09 08:09:32 -0500

    bubble sort added

---
+++ sort/bubbl.py
@@ -0,0 +1,11 @@
+# initialize numerical data that needs to be sorted
+
+# copy the integer arrays
+# print current, unsorted state
+
+# begin sort:
+# two loops, nested together
+# outer loop ___
+# inner loop ___
+# elements are ___
+# print each step
